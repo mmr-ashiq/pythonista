bfdeb5126e6ec82ac884bee682def38e098fa625
Md. Mizanur Rahman Ashiq <mizanur35-2844@diu.edu.bd>
2022-03-21 14:09:41 +0600

    Heart symbol with turle:javascript is love:

---
+++ 1. Holla/turtle_love_404.py
@@ -0,0 +1,54 @@
+import turtle
+
+pen = turtle.Turtle()
+
+
+def curve():
+    for i in range(200):
+        pen.right(1)
+        pen.forward(1)
+
+
+def heart():
+    pen.fillcolor('red')
+
+    pen.begin_fill()
+
+    pen.speed(1)
+
+    pen.left(140)
+    pen.forward(113)
+
+    curve()
+    pen.left(120)
+
+    curve()
+
+    pen.forward(112)
+
+    pen.end_fill()
+
+
+def txt():
+
+    pen.up()
+
+    pen.setpos(-68, 95)
+
+    pen.down()
+
+    pen.color('white')
+
+    pen.write("Javascript", font=(
+        "Arial", 12, "bold")
+    )
+
+    pen.ht()
+    turtle.done()
+
+
+heart()
+
+txt()
+
+pen.ht()
